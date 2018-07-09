ee7b0e36d1a9c64916bf13b49264dcfcfc308697
Philip Mes <fmesy@outlook.com>
2018-07-09 00:51:06 -0300

    Observable

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,12 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
-  indredientsChanged = new EventEmitter<Ingredient[]>();
+  indredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -20,14 +21,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.indredientsChanged.emit(this.ingredients.slice());
+    this.indredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
-    // for (let ingredient of ingredients) {
-    //   this.addIngredient(ingredient);
-    // }
     this.ingredients.push(...ingredients);
-    this.indredientsChanged.emit(this.ingredients.slice());
+    this.indredientsChanged.next(this.ingredients.slice());
   }
 }
